f3ca2a984c3e289f57adc1f4a7c892fc79a868f5
Antonin Hotte <antonin.hotte@gmail.com>
2026-04-14 12:10:31 +0100

    commit initial

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script setup>
+const buildId = import.meta.env.VITE_BUILD_ID ?? 'local'
+const today = new Date().toLocaleDateString('fr-FR')
+</script>
+<template>
+<header>
+<p>Nom : Antonin Hotte</p>
+<p>Date : {{ today }}</p>
+<p>Build : {{ buildId }}</p>
+</header>
+</template>
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  background-color: white;
+}
+
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+
+p {
+  color: black;
+  font-size: 1.5rem;
+}
+</style>
